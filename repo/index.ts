6d280ddefd8abe7d9f314239f5bbc672ff9158f2
Erik Rasmussen <ecr.referee@hotmail.com>
2020-12-19 14:01:27 -0600

    Format index.ts

--- repo/index.ts
+++ repo/index.ts
@@ -1,19 +1,19 @@
 import * as github from '@pulumi/github';
 
 export const defaultRepoOptions: github.RepositoryArgs = {
-    allowMergeCommit: false,
-    allowRebaseMerge: true,
-    allowSquashMerge: true,
-    autoInit: true,
-    deleteBranchOnMerge: true,
-    gitignoreTemplate: 'Node',
-    hasIssues: true,
-    hasProjects: false,
-    hasWiki: false,
-    licenseTemplate: 'wtfpl',
+  allowMergeCommit: false,
+  allowRebaseMerge: true,
+  allowSquashMerge: true,
+  autoInit: true,
+  deleteBranchOnMerge: true,
+  gitignoreTemplate: 'Node',
+  hasIssues: true,
+  hasProjects: false,
+  hasWiki: false,
+  licenseTemplate: 'wtfpl',
 };
 
 export function safirRepo(name: string, args: github.RepositoryArgs = defaultRepoOptions): github.Repository {
-    // Name should be overwritten if supplied
-    return new github.Repository(name, { name, ...defaultRepoOptions, ...args });
-};
+  // Name should be overwritten if supplied
+  return new github.Repository(name, { name, ...defaultRepoOptions, ...args });
+}

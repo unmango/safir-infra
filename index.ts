1dafe30e2f392124b91a9262cf50d3d2c647cd57
Erik Rasmussen <ecr.referee@hotmail.com>
2020-12-18 21:19:31 -0600

    Add fn and default options for safir repos

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
+import * as github from '@pulumi/github';
 import * as pulumi from "@pulumi/pulumi";
+
+const defaultRepoOptions: github.RepositoryArgs = {
+    allowMergeCommit: false,
+    allowRebaseMerge: true,
+    allowSquashMerge: true,
+    autoInit: true,
+    deleteBranchOnMerge: true,
+    gitignoreTemplate: 'wtfpl',
+    hasIssues: true,
+    hasProjects: false,
+    hasWiki: false,
+    licenseTemplate: 'wtfpl',
+};
+
+function safirRepo(name: string, args: github.RepositoryArgs = defaultRepoOptions): github.Repository {
+    // Name should be overwritten if supplied
+    return new github.Repository(name, { name, ...defaultRepoOptions, ...args });
+};

d0732a79865e805ab9f3cb3c04d228ea7936678e
Erik Rasmussen <ecr.referee@hotmail.com>
2020-12-19 19:06:06 -0600

    Add cli repo

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from '@pulumi/pulumi';
+import { safirRepo } from './repo';
+
+const cli = safirRepo('cli', {
+  description: 'The Safir CLI tool',
+  gitignoreTemplate: 'VisualStudio',
+});

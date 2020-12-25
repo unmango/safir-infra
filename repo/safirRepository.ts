fde7a3bffd130039263e0b147295089e0c87d01a
Erik Rasmussen <ecr.referee@hotmail.com>
2020-12-25 14:08:32 -0600

    Add safirRepository component resource

---
+++ repo/safirRepository.ts
@@ -0,0 +1,26 @@
+import * as github from '@pulumi/github';
+import * as pulumi from '@pulumi/pulumi';
+import { safirRepo } from './index';
+
+export class SafirRepository extends pulumi.ComponentResource {
+
+  public readonly repo: github.Repository;
+  public readonly files: github.RepositoryFile[];
+
+  constructor(
+    private name: string,
+    private args?: SafirRepositoryArgs,
+    private opts?: pulumi.ComponentResourceOptions) {
+    super('unmango:safir:repo', name, undefined, opts);
+
+    this.repo = safirRepo(this.name, this.args);
+
+    this.registerOutputs();
+  }
+
+}
+
+export interface SafirRepositoryArgs {
+  description?: string;
+  gitignoreTemplate?: 'VisualStudio' | 'Node';
+}

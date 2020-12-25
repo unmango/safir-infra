ba8d8358ba7a8a07377479c23d2c8948aafcbf9a
Erik Rasmussen <ecr.referee@hotmail.com>
2020-12-25 16:34:55 -0600

    Refactor out of function

--- repo/safirRepository.ts
+++ repo/safirRepository.ts
@@ -2,18 +2,36 @@ import * as github from '@pulumi/github';
 import * as pulumi from '@pulumi/pulumi';
 import { safirRepo } from './index';
 
+const defaultRepoOptions: github.RepositoryArgs = {
+  allowMergeCommit: false,
+  allowRebaseMerge: true,
+  allowSquashMerge: true,
+  autoInit: true,
+  deleteBranchOnMerge: true,
+  gitignoreTemplate: 'Node',
+  hasIssues: true,
+  hasProjects: false,
+  hasWiki: false,
+  licenseTemplate: 'wtfpl',
+};
+
 export class SafirRepository extends pulumi.ComponentResource {
 
   public readonly repo: github.Repository;
   public readonly files: github.RepositoryFile[] = [];
 
   constructor(
-    private name: string,
-    private args?: SafirRepositoryArgs,
-    private opts?: pulumi.ComponentResourceOptions) {
+    name: string,
+    args?: SafirRepositoryArgs,
+    opts?: pulumi.ComponentResourceOptions) {
     super('unmango:safir:repo', name, undefined, opts);
+    const sfName = `safir-${name}`;
 
-    this.repo = safirRepo(this.name, this.args, { parent: this });
+    this.repo = new github.Repository(sfName, {
+      name: sfName,
+      ...defaultRepoOptions,
+      ...args,
+    }, { parent: this });
 
     this.registerOutputs();
   }

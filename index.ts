deb2aa0fd954a55af72385a97ed079211701cfdf
UnstoppableMango <ecr.referee@hotmail.com>
2020-12-25 19:28:02 -0600

    Add manager repo (#12)

--- index.ts
+++ index.ts
@@ -5,3 +5,8 @@ const cli = new SafirRepository('cli', {
   description: 'The Safir CLI tool',
   gitignoreTemplate: 'VisualStudio',
 });
+
+const manager = new SafirRepository('manager', {
+  description: 'Safir manager service',
+  gitignoreTemplate: 'VisualStudio',
+});
